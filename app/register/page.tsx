a1e29b9161c16ceb5bca229db4ad7870f54a915c
Eduardo Trein - E. Escolinha de TI - BNE <seu.email@exemplo.com>
2025-09-11 07:59:10 -0300

    FIX: cor register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,7 @@ import AuthForm from "../components/AuthForm";
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-8">
+    <div className="min-h-screen flex items-center justify-center p-8 bg-black">
       <AuthForm mode="register" />
     </div>
   );
